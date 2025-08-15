6c8e9d7cdfe12824b2bc0a645b980b96a984b9ee
PEACHS481 <J271481@tafe.wa.edu.au>
2025-08-15 23:22:56 +0800

    Update player_test.py

--- test/player_test.py
+++ test/player_test.py
@@ -3,10 +3,10 @@ import unittest
 from player import Player
 
 class TestPlayerClass(unittest.Testcase):
-    def add_player_with_uid(self):
+    def test_add_player_with_uid(self):
         player1 = Player("J271481", None)
         self.assertEqual(player1.uid, "J271481")
 
-    def add_player_with_name(self):
+    def test_add_player_with_name(self):
         player1 = Player(None, "Myself")
         self.assertEqual(player1.name, "Myself")
