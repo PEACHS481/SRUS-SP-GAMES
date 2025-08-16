f8043ca6f25326ed4102d7ec680ccbbe28dfc6aa
PEACHS481 <J271481@tafe.wa.edu.au>
2025-08-16 15:18:17 +0800

    Step 7.C commit

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -17,4 +17,25 @@ class Test_player_list(unittest.TestCase):
         self.assertEqual(pl1.head.player.name, "Sam", "head should point to Sam player node" )
         self.assertEqual(pl1.tail.player.name, "Sam", "tail should point to Sam player node")
 
+    def test_append_to_empty_list(self):
+        pl1 = PlayerList()
 
+        self.assertTrue(pl1.is_empty(), "Expected Return: List is Empty")
+        person1 = Player("001", "Sam")
+        pl1.append(person1)
+
+        self.assertFalse(pl1.is_empty(), "Expected Return :List should not be empty")
+        self.assertEqual(pl1.head.player.name, "Sam", "Head is pointing to Sam")
+        self.assertEqual(pl1.tail.player.name, "Sam", "Tail is pointing at Sam")
+
+    def tes_append_to_non_empty_list(self):
+        pl1 = PlayerList()
+        person1 = Player("001", "Sam")
+        person2 = Player("002", "Tony")
+        pl1.append(person1)
+        pl1.append(person2)
+
+        self.assertEqual(pl1.head.player.name, "Sam", "Head pointing at Sam")
+        self.assertEqual(pl1.tail.player.name, "Tony", "Tail pointing at Tony")
+        self.assertEqual(pl1.head.next_node.player.name, "Tony", "Sam should be linking next to Tony")
+        self.assertEqual(pl1.head.prev_node.player.name, "Sam", "Tony should be linking prev to Sam")
