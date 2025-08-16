7dfe3055f0f39ee0b130f9b9b1375e837e366573
PEACHS481 <J271481@tafe.wa.edu.au>
2025-08-16 14:27:51 +0800

    Step 6 Commit

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -1,4 +1,3 @@
-
 import unittest
 
 from app.player import Player
@@ -8,10 +7,14 @@ class Test_player_list(unittest.TestCase):
     def test_player_list_is_empty_true(self):
         pl1 = PlayerList()
         self.assertTrue(pl1.is_empty(), "Expected Return: True thats it is indeed a empty list")
+        self.assertIsNone(pl1.tail, "Expected Return: tail is none and list is empty")
 
     def test_player_list_is_empty_false(self):
         pl1 = PlayerList()
         person1 = Player("001", "Sam")
         pl1.push(person1)
         self.assertFalse(pl1.is_empty(), "Expected Return: False for a empty list")
+        self.assertEqual(pl1.head.player.name, "Sam", "head should point to Sam player node" )
+        self.assertEqual(pl1.tail.player.name, "Sam", "tail should point to Sam player node")
+
 
