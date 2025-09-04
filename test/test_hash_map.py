836d0785602b593eb4d112074caeb26514e2ddfe
SamuelPeach <j271481@tafe.wa.edu.au>
2025-09-04 16:42:08 +0800

    add 2 unit tests

--- test/test_hash_map.py
+++ test/test_hash_map.py
@@ -2,10 +2,9 @@
 # Author Samuel Peach
 
 import unittest
+from unittest.mock import patch
 
-from app.player import Player
-from app.player_list import PlayerList, PlayerHashMap
-from app.player_node import PlayerNode
+from app.player_list import PlayerHashMap
 
 
 class TestHashMap(unittest.TestCase):
@@ -61,6 +60,44 @@ class TestHashMap(unittest.TestCase):
         self.assertEqual(updated_player.name, "Samuel")
 
         self.assertEqual(len(self.hashmap), 1)
+
+
+    def test_collision_handling(self):
+        """Basic collision test, Both players should collide, as they will both be in the same playerList (5)."""
+        self.hashmap["001"] = "Sam"
+        self.hashmap["010"] = "Tony"
+
+        player1 = self.hashmap["001"]
+        player2 = self.hashmap["010"]
+
+        self.assertEqual(player1.name, "Sam")
+        self.assertEqual(player2.name, "Tony")
+
+        # self.hashmap.display() //testing
+
+        playerlist = self.hashmap._map[5]
+
+        # search through list to see they are added
+        players_in_list = []
+        current_node = playerlist.head
+        while current_node:
+            players_in_list.append(current_node.player)
+            current_node = current_node.next_node
+
+        self.assertEqual(players_in_list[0].name, "Sam")
+        self.assertEqual(players_in_list[1].name, "Tony")
+
+
+    def test_initial_hashmap(self):
+        """Checking the state of the hashmap on initialization"""
+
+        # Check it has zero players
+        self.assertEqual(len(self.hashmap), 0)
+
+        # Size of hashmap is equal to size defined
+        self.assertEqual(self.hashmap.len_lists(), 10)
+
+
 
 if __name__ == '__main__':
     unittest.main()
